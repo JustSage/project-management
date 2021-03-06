2965dda27309a16346404411a7dc45d75eb5b4fc
YehonatanHen <yonatan.h333@gmail.com>
2021-03-06 12:39:42 +0200

    Form value configured as state values

--- src/components/login-and-signup-components/login.jsx
+++ src/components/login-and-signup-components/login.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+// import axios from "axios";
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            password: "",
+            username: ""
+        };
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUsername = this.handleUsername.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+    }
+
+    handlePassword = (event) => {
+        this.setState({password: event.target.value});
+    }
+
+    handleUsername = (event) => {
+        this.setState({username: event.target.value});
+    }
+
+    handleSubmit = () => {
+        alert(this.state.username);
     }
 
     render() {
         return (
             <>
                 <h1>Login page</h1>
-                <form action="" method="get">
-                   <input type="text" placeholder="Enter username"/>
-                   <input type="password" placeholder="Enter password"/>
+                <form onSubmit={this.handleSubmit} method="get">
+                   <input type="text" placeholder="Enter username" value={this.state.username} onChange={this.handleUsername}/>
+                   <br/><br/>
+                   <input type="password" placeholder="Enter password" value={this.state.password} onChange={this.handlePassword}/>
+                   <br/><br/>
                    <button type="submit">Log in!</button>
                 </form>
             </>
