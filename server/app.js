684d0834524aea3dc9c8b39a7328a45e7a593ae2
YehonatanHen <yonatan.h333@gmail.com>
2021-03-06 12:51:58 +0200

    fix bug in server/app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const userRouter = require("./routers/user");
+const userRouter = require("./routers/user")
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -8,9 +8,7 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3001;
 
 app.use(express.static(publicDirectoryPath));
-app.use(express.json()); // Parses request body if type is json. Saves to req.body.
 app.use(userRouter);
-
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
