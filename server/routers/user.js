ac3be69291b28c9ddf4550f4da6f60044cfdf79c
YehonatanHen <yonatan.h333@gmail.com>
2021-03-09 19:47:46 +0200

    Validation of login with email created in backend

--- server/routers/user.js
+++ server/routers/user.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 const express = require('express')
 const router = new express.Router()
+const validator = require('validator')
 
 const userData = {
 	username: 'Admin',
@@ -11,8 +12,9 @@ const userData = {
 router.post('/login', async (req, res) => {
 	const user = { username: req.body.username, password: req.body.password }
 	if (
-		user.username === userData.username &&
-		user.password === userData.password
+		user.password === userData.password &&
+		(user.username === userData.username ||
+			(user.username === userData.email && validator.isEmail(user.username))) //Validation of email format
 	) {
 		res.status(200).send({ message: 'logged in successfully!' })
 	} else res.status(404).send()
