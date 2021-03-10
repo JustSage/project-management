e036d139052f62493af878b4d0bb3966b8e091dc
RabbitJazz <sagebaram@gmail.com>
2021-03-10 18:21:08 +0200

    added email validation, username and password tests

--- test/tests.test.js
+++ test/tests.test.js
@@ -3,14 +3,13 @@ const request = require('supertest')
 const app = require('../server/app')
 const validator = require('validator')
 
-// test('Hello world!!', () => {})
-
 const userOne = {
 	name: 'Admin',
 	email: 'admin@gmail.com',
 	password: 'Admin123',
 }
 
+// Checks if post request passes
 test('Check user', async () => {
 	await request(app)
 		.post('/login')
@@ -21,12 +20,14 @@ test('Check user', async () => {
 		.expect(200)
 })
 
-//TODO
-test('Validate email', () => {})
+test('Validate email', () => {
+	expect(validator.isEmail(userOne.email).toBe(true))
+})
 
-//TODO - isAlphaNumeric
-test('Check if password include letters', () => {})
+test('Check if username include letters', () => {
+	expect(validator.isAlpha(userOne.user)).toBe(true)
+})
 
-test('Check if username include letters and numbers', () => {
-	expect(validator.isAlpha(userOne.name)).toBe(true)
+test('Check if password include letters and numbers', () => {
+	expect(validator.isAlphaNumeric(userOne.password)).toBe(true)
 })
