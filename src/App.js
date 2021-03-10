1ba0757344cbe90318b0663d90dc2d60c6ef028c
ariel <ariel7590@gmail.com>
2021-03-10 02:11:19 +0200

    modified a little the homepage and the navbar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Login from './components/login-and-signup-components/login'
 import Homepage from './components/general-components/homepage'
 import SignUp from './components/login-and-signup-components/sign-up'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)
 function App() {
 	return (
 		<>
 			<div className='bg'>
 				<Router>
 					<Switch>
-						<Route exact path='/' component={Login} />
+						<Route exact path='/' component={Homepage} />
 						<Route path='/login' component={Login} />
 						<Route path='/signUp' component={SignUp} />
 						<Route path='/homepage' component={Homepage} />
