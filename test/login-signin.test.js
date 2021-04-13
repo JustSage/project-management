9950ef861c6c1fe2dbd5540bac4c5fc96c0934d7
YehonatanHen <yonatan.h333@gmail.com>
2021-04-13 18:04:43 +0300

    login-signin tests created

--- test/login-signin.test.js
+++ test/login-signin.test.js
@@ -2,7 +2,7 @@
 const request = require('supertest')
 const app = require('../server/app-source')
 
-test('Should signup a new user', async () => {
+test('Signup as a new user with appropriate details.', async () => {
 	await request(app)
 		.post('/sign-up')
 		.send({
@@ -13,4 +13,24 @@ test('Should signup a new user', async () => {
 			role: 'Customer',
 		})
 		.expect(200)
+})
+
+test('Access the homepage as a user (test user which created above)', async () => {
+	await request(app)
+		.post('/login')
+		.send({
+			username: 'test user',
+			password: 'test password',
+		})
+		.expect(200)
+})
+
+test('Access the homepage as non-user', async () => {
+	await request(app)
+		.post('/login')
+		.send({
+			username: 'non-test user',
+			password: 'non-test password',
+		})
+		.expect(404)
 })
