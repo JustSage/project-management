6dcc1431364d5e9841a018a725f12da60078911c
Dave-Sama <davech1228@gmail.com>
2021-04-14 21:49:53 +0300

    Hotels component added

---
+++ src/components/general-components/Customer/Hotels.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+/*
+    the idea behind this component is:
+
+    a list of available Hotel deals to various of places around the world.
+
+    from my perspective (Dave), it should be a boundle of much smaller components, like a Calendar, courosel of hotels in a specific place, form, and more.
+*/
+
+export default class Hotels extends Component {
+	render() {
+		return <div>Hotels</div>
+	}
+}
