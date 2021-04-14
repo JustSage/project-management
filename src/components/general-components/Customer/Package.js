9e65086b04c2833d689364d70617c66dc94aae57
Dave-Sama <davech1228@gmail.com>
2021-04-14 21:50:21 +0300

    Packages component added

---
+++ src/components/general-components/Customer/Package.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+/* 
+	the main idea behind Package Component:
+
+	this component will represent Packages containing flights and hotels and rent car services in different places.
+
+	it should be a boundle of smaller and reusable components, like card, courusel and much more.
+
+*/
+export default class Package extends Component {
+	render() {
+		return <div>Packages</div>
+	}
+}
