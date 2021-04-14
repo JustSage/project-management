c29b7a8d3e994ae35543f3efdb8853766e3a4a5e
Dave-Sama <davech1228@gmail.com>
2021-04-14 21:47:57 +0300

    car component added

---
+++ src/components/general-components/Customer/Car.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+/*
+    the idea behind this component is:
+
+    a list of available rental Car deals in various of places around the world.
+
+    from my perspective (Dave), it should be a boundle of much smaller components, like a Calendar, courosel of available cars, form, and more.
+*/
+
+export default class Car extends Component {
+	render() {
+		return <div>Car</div>
+	}
+}
