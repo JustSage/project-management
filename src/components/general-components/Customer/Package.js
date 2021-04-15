84f7de9ea2fac753ea54825571850ef10176a79d
Dave-Sama <davech1228@gmail.com>
2021-04-15 03:26:27 +0300

    package explanation added

--- src/components/general-components/Customer/Package.js
+++ src/components/general-components/Customer/Package.js
@@ -12,15 +12,37 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 */
 export default class Package extends Component {
+	state = {
+		location: '',
+		packages: null, // will hold our elements that we will receive from the db
+	}
+	handleLocation = (event) => {
+		event.preventDefault()
+		this.setState({ location: event.target.value })
+
+		// at this part we can make a search in the DB to look for a match -> if we found a match -> add to packages .
+		// after we finished with the db check, we need to pass the info we got to the <Card /> component:
+
+		// like this: <Card: locationName= {} context = {} image = {} />
+
+		// after this we will map our elements and display them as jsx.
+	}
+
 	render() {
 		return (
 			<div className='package'>
 				<div className='wrraper'>
 					<label className='packageLabel' htmlFor='location'>
-						Choose a Location:
+						Choose Location:
 					</label>
-					<input className='packageInput' id='location' />
+					<input
+						onChange={this.handleLocation}
+						className='packageInput'
+						id='location'
+					/>
 				</div>
+				<br />
+				{this.state.found != 0 ? this.state.packages : null}
 			</div>
 		)
 	}
