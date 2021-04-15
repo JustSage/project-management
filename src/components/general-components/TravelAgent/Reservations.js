08b2433697b8b1a8fd2d109911db323d4c75908d
YehonatanHen <yonatan.h333@gmail.com>
2021-04-15 10:58:10 +0300

    Merge branch 'dev/yehonatan' into development

---
+++ src/components/general-components/TravelAgent/Reservations.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+/* 
+    The idea behind this component:
+
+    a list of reservations that have been made by the customer, only the travel agent/admin could see this list of course,
+
+    this component will be the base for the requrement : "status: "in process/approved/canceled."
+*/
+export default class Reservations extends Component {
+	render() {
+		return <div>Reservations</div>
+	}
+}
