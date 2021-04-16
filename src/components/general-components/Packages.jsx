331add3946e4f0eebefa38bb1cbfa112c3101085
YehonatanHen <yonatan.h333@gmail.com>
2021-04-16 08:55:20 +0300

    Edited packages template page

--- src/components/general-components/Packages.jsx
+++ src/components/general-components/Packages.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react'
+import NavbarComponent from '../general-components/navbar'
+import Sidebar from '../general-components/sidebar'
 /**
  * Packages page which appears as navbar tag, when click on the navbar button it'll be redirecting here
  * by routing. This page includes the package-components.
@@ -7,7 +10,14 @@ class Packages extends Component {
 	render() {
 		return (
 			<>
-				<div>Packages</div>
+				<NavbarComponent history={this.props.history} />
+
+				<div className='row hp-divs'>
+					<div className='col-xs-12'>
+						<Sidebar history={this.props.history} />
+					</div>
+				</div>
+				<h3>Packages</h3>
 			</>
 		)
 	}
