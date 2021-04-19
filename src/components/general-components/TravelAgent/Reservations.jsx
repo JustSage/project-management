5f8db8446b04ff7c5af42a3d3b4fbfd76163cabb
Dave-Sama <davech1228@gmail.com>
2021-04-19 20:48:29 +0300

    reservations compornent added

---
+++ src/components/general-components/TravelAgent/Reservations.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Reservations extends Component {
+	render() {
+		return (
+			<div>
+				<h3>Reservations</h3>
+			</div>
+		)
+	}
+}
