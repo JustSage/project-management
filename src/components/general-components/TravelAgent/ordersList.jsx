05fec740762e7c005f93c5f91e19046c511cf2aa
YehonatanHen <yonatan.h333@gmail.com>
2021-04-19 20:30:01 +0300

    Orders list component modified

--- src/components/general-components/TravelAgent/ordersList.jsx
+++ src/components/general-components/TravelAgent/ordersList.jsx
@@ -1,12 +1,26 @@
+import axios from 'axios'
+import { response } from 'express'
 import React, { Component } from 'react'
-/* 
-    The idea behind this component:
-
-    a list of reservations that have been made by the customer, only the travel agent/admin could see this list of course,
-
-    this component will be the base for the requrement : "status: "in process/approved/canceled."
-*/
+/**
+ * Component represents the orders which was made by customers. 
+ * Orders will represented with their full details and includes sorting options that will be eased the users to inspect the data.
+ */
 export default class ordersList extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			orders: [],
+		}
+		axios
+			.get('orders')
+			.then((response) => {
+				this.state.orders.push(response.data.orders)
+			})
+			.catch((error) => {
+				console.log(error.response.data.message)
+				alert(error.response.data.message)
+			})
+	}
 	render() {
 		return <div>Orders</div>
 	}
