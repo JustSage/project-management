ced5c42f429eec63a41e9828122c96f4cf9d4487
Dave-Sama <davech1228@gmail.com>
2021-04-19 21:47:42 +0300

    reservation component added

--- src/components/general-components/TravelAgent/Reservations.jsx
+++ src/components/general-components/TravelAgent/Reservations.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-
+import { Container } from 'react-bootstrap'
 export default class Reservations extends Component {
 	render() {
 		return (
-			<div>
-				<h3>Reservations</h3>
-			</div>
+			<Container>
+				<div>
+					<h3>Reservations</h3>
+				</div>
+			</Container>
 		)
 	}
 }
