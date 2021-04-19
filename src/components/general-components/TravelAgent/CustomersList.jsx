a403351861673fa59886f6dca95030baea8dcb7e
RabbitJazz <sagebaram@gmail.com>
2021-04-19 17:18:11 +0300

    initialized customer list requirement #302

--- src/components/general-components/TravelAgent/CustomersList.jsx
+++ src/components/general-components/TravelAgent/CustomersList.jsx
@@ -1,4 +1,59 @@
 import React, { Component } from 'react'
+import { Modal } from 'react-bootstrap'
+const { MongoClient } = require('mongodb')
+const URL = process.env.MONGODB_URL
+
+MongoClient.connect(URL, async (error, client) => {
+    if (error) throw error
+    var customers = client.db(process.env.DATABASE_NAME)
+    customers.collection('customers').find({role:"Customer"}).toArray(function(error,result){
+        if (error) throw error
+        console.log(result)
+        customers.close()
+    })
+})
+
+function CustomerServiceModal(props) {
+    return (
+      <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+              Agent's Customer Service Modal
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Customer List</h4>
+            <div>
+            {customers.map(function(customer, index){
+            return (<li key={index}>{customer}</li>)
+            })}
+            </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+  
+  function App() {
+    const [modalShow, setModalShow] = React.useState(false);
+  
+    return (
+      <>
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          Customer List
+        </Button>
+  
+        <CustomerServiceModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+      </>
+    );
+  }
+  
+  render(<App />);
 
 /* The main idea for this components: 
    A list of Customers which will be represented to the Travel agent/Admin.
@@ -7,8 +62,10 @@ import React, { Component } from 'react'
    
     the list can be displayed as a combobox or something -> be creative.
     */
-export default class CustomersList extends Component {
+class CustomersList extends Component {
 	render() {
-		return <div>Customers List</div>
-	}
+        <div>List Of Customers</div>
+    }
 }
+
+export default CustomersList
