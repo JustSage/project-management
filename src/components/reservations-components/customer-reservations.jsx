a3b1595e42b40987b311bfde724e9d7f18085451
Dave-Sama <davech1228@gmail.com>
2021-04-23 21:22:07 +0300

    a page for the customer, in it he could track/monitor his reservations

---
+++ src/components/reservations-components/customer-reservations.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Container } from 'react-bootstrap'
+export default class CustomeReservations extends Component {
+	/*
+	
+	*/
+	render() {
+		return (
+			<Container>
+				<div>
+					<h3>customer-reservations</h3>
+				</div>
+			</Container>
+		)
+	}
+}
