1000bc62ed6c4e10bea513d9e3a196fb8b8c3103
Dave-Sama <davech1228@gmail.com>
2021-04-25 22:53:42 +0300

    in travel-agent-reservations i've fetched the data from orders Collection and inserted it into the trave-agent table

--- src/components/reservations-components/travel-agent-reservations.jsx
+++ src/components/reservations-components/travel-agent-reservations.jsx
@@ -16,37 +16,11 @@ export default class TAReservations extends Component {
 
 	state = {
 		data: undefined,
-		demoCustomers: [
-			{
-				fullName: 'David Charon-Zade',
-				destination: 'Canada',
-				deal: 'Package',
-				price: '1000$',
-			},
-			{
-				fullName: 'Yehonatan Ben',
-				destination: 'Canada',
-				deal: 'Flght',
-				price: '1000$',
-			},
-			{
-				fullName: 'Ariel Turchinsky',
-				destination: 'Canada',
-				deal: 'Package',
-				price: '1000$',
-			},
-			{
-				fullName: 'Saggy Balls',
-				destination: 'Canada',
-				deal: 'Flight',
-				price: '1000$',
-			},
-		],
 	}
 
 	componentDidMount() {
 		axios
-			.get('/pending-reservations')
+			.get('/orders')
 			.then((response) => {
 				this.setState(
 					{
@@ -68,7 +42,7 @@ export default class TAReservations extends Component {
 		this.setState({ demoCustomers: temp })
 	}
 
-	acceptAll = () => {
+	handleAcceptAll = () => {
 		// logic for empty the db
 		let temp
 		while (this.state.demoCustomers.length > 0) {
@@ -78,44 +52,61 @@ export default class TAReservations extends Component {
 		this.setState({ demoCustomers: temp })
 	}
 	render() {
-		return (
-			<div>
-				<Table striped bordered hover>
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Full Name</th>
-							<th>Destination</th>
-							<th>Deal</th>
-							<th>Price</th>
-							<th>Approve</th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.demoCustomers.map((element, index) => {
-							return (
-								<tr key={index}>
-									<td>{index + 1}</td>
-									<td>{element.fullName}</td>
-									<td>{element.destination}</td>
-									<td>{element.deal}</td>
-									<td>{element.price}</td>
-									<td>
-										<Button
-											onClick={() => {
-												this.handleAccept(index)
-											}}
-										>
-											Accept
-										</Button>
-									</td>
-								</tr>
-							)
-						})}
-					</tbody>
-				</Table>
-				<Button onClick={this.acceptAll}>Accpet All</Button>
-			</div>
-		)
+		if (this.state.data === undefined) {
+			return (
+				<div className='text-center'>
+					<div className='spinner-border' role='status'>
+						<span className='sr-only'>Loading...</span>
+					</div>
+				</div>
+			)
+		} else
+			return (
+				<div>
+					<Table striped bordered hover>
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Full Name</th>
+								<th>Destination</th>
+								<th>Price</th>
+								<th>Deal</th>
+								<th>Status</th>
+								<th>Order Date</th>
+								<th>Start</th>
+								<th>End</th>
+								<th>Approve</th>
+							</tr>
+						</thead>
+						<tbody>
+							{this.state.data.map((element, index) => {
+								return (
+									<tr key={index}>
+										<td>{index + 1}</td>
+										<td>{element.User}</td>
+										<td>{element.Destination}</td>
+										<td>{element.Price}</td>
+										<td>{element.Deal}</td>
+										<td>{element.Status}</td>
+										<td>{element.OrderDate}</td>
+										<td>{element.Start}</td>
+										<td>{element.End}</td>
+										<td>
+											<Button
+												onClick={() => {
+													this.handleAccept(index)
+												}}
+											>
+												Accept
+											</Button>
+										</td>
+									</tr>
+								)
+							})}
+						</tbody>
+					</Table>
+					<Button onClick={this.handleAcceptAll}>Accpet All</Button>
+				</div>
+			)
 	}
 }
