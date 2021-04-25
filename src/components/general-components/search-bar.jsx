fd0543150fda8664f7e565b91bc9d560daf86daa
ariel <ariel7590@gmail.com>
2021-04-25 23:25:24 +0300

    Search bar now takes results from db

--- src/components/general-components/search-bar.jsx
+++ src/components/general-components/search-bar.jsx
@@ -1,22 +1,63 @@
 import react from 'react'
 import '../../css/search-bar.css'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class SearchBar extends react.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			data: undefined,
+		}
+		axios
+			.get('/packages')
+			.then((response) => {
+				this.setState({
+					data: response.data,
+				})
+			})
+			.catch((error) => {
+				console.log(error.response.data.message)
+				alert(error.response.data.message)
+			})
+	}
+
 	render() {
-		return (
-			<>
-				<input
-					type='search'
-					placeholder='Search for packages...'
-					className='search-input'
-					list='cityname'
-				/>
-				<datalist id='cityname'>
-					<option value='Athens' />
-					<option value='Ashkelon' />
-				</datalist>
-			</>
-		)
+		if (this.state.data === undefined) {
+			return (
+				<div className='text-center'>
+					<div className='spinner-border' role='status'>
+						<span className='sr-only'>Loading...</span>
+					</div>
+				</div>
+			)
+		} else {
+			return (
+				<>
+					<input
+						type='search'
+						placeholder='Search for packages...'
+						className='search-input'
+						list='cityname'
+					/>
+					<datalist id='cityname'>
+						{this.state.data.map((pkg) => {
+							return (
+								<div key={pkg.description}>
+									<Link
+										to={{
+											pathname: `/make-order/${this.props.name}/${this.props.price}/${this.props.description}`,
+										}}
+									>
+										<option value={pkg.name}>{pkg.price}</option>
+									</Link>
+								</div>
+							)
+						})}
+					</datalist>
+				</>
+			)
+		}
 	}
 }
 
