c5572e00c80625f8bca2692e6c2d08ade7f0234a
YehonatanHen <yonatan.h333@gmail.com>
2021-05-09 20:15:33 +0300

    Destinations list added to homepage

--- src/components/general-components/homepage-components/homecontext.jsx
+++ src/components/general-components/homepage-components/homecontext.jsx
@@ -1,22 +1,52 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable prettier/prettier */
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../../../css/homepage.css'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, ListGroup } from 'react-bootstrap'
+import axios from 'axios'
 
 class HomeContext extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			destinations: undefined,
+		}
+
+		axios
+			.get('/packages')
+			.then((response) => {
+				const names = response.data.map((pkg) => pkg.name)
+				this.setState({ destinations: names })
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
 	render() {
-		return (
-			<>
-				<Container fluid>
-					<Row>
-						<Col sm={2}>list will be here</Col>
-						<Col sm={10}>map will be here</Col>
-					</Row>
-				</Container>
-			</>
-		)
+		if (this.state.destinations == undefined)
+			return (
+				<div className='text-center'>
+					<div className='spinner-border' role='status'>
+						<span className='sr-only'>Loading...</span>
+					</div>
+				</div>
+			)
+		else
+			return (
+				<>
+					<Container fluid>
+						<Row>
+							<Col sm={2}>
+								<ListGroup variant='flush'>
+									{this.state.destinations.map((dest, i) => {
+										return <ListGroup.Item key={i}>{dest}</ListGroup.Item>
+									})}
+								</ListGroup>
+							</Col>
+							<Col sm={10}>map will be here</Col>
+						</Row>
+					</Container>
+				</>
+			)
 	}
 }
 
