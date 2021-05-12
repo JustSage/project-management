d01790eb1471efb000a41216e7eb362c15b2cbad
Dave-Sama <davech1228@gmail.com>
2021-05-12 09:14:53 +0300

    send-broadcast-email functionality added, now admin/TA can send a broadcast email to all customers

--- server/routers/email.js
+++ server/routers/email.js
@@ -28,5 +28,34 @@ router.post('/send-email', async (req, res) => {
 		res.status(500).send({ message: "Can't! send an email" })
 	}
 })
+router.post('/send-broadcast-email', async (req, res) => {
+	const email = req.body
+	const emails = email.emails
+	const text = email.text
+
+	console.log(`text is: \n ${text}`)
+	console.log(`emails are: \n ${emails}`)
+	let msg
+
+	try {
+		msg = {
+			to: emails,
+			from: 'davech1228@gmail.com',
+			subject: 'Broadcast Message!',
+			text: text,
+		}
+
+		sgMail.sendMultiple(msg, (err) => {
+			if (err) {
+				console.log('email did not sent')
+			} else {
+				console.log(`email sent successfully to ${emails}`)
+			}
+		})
+	} catch (e) {
+		console.log(e)
+		res.status(500).send({ message: "Can't! send an email" })
+	}
+})
 
 module.exports = router
