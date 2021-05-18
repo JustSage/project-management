d5d13397d0b87ea1b4b5ef6d22ffacf1a7293cf2
ariel <ariel7590@gmail.com>
2021-05-19 02:46:28 +0300

    Made the read message into modal

--- src/components/inbox-components/inbox.jsx
+++ src/components/inbox-components/inbox.jsx
@@ -1,65 +1,74 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import Table from 'react-bootstrap/Table'
-import ReadMessage from './read-message'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 
-class Inbox extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			data: undefined,
-		}
-		axios
-			.get('/messages')
-			.then((response) => {
-				this.setState({
-					data: response.data,
-				})
-				console.log(response.data)
-			})
-			.catch((error) => {
-				console.log(error.response.data.message)
-				alert(error.response.data.message)
-			})
-	}
+function Inbox() {
+	const [data, setData] = useState(undefined)
+	const [show, setShow] = useState(false)
 
-	render() {
-		if (this.state.data === undefined) {
-			return (
-				<div className='text-center'>
-					<div className='spinner-border' role='status'>
-						<span className='sr-only'>Loading...</span>
-					</div>
+	const handleClose = () => setShow(false)
+	const handleShow = () => setShow(true)
+	axios
+		.get('/messages')
+		.then((response) => {
+			setData(response.data)
+			console.log(response.data)
+		})
+		.catch((error) => {
+			console.log(error.response.data.message)
+			alert(error.response.data.message)
+		})
+
+	if (data === undefined) {
+		return (
+			<div className='text-center'>
+				<div className='spinner-border' role='status'>
+					<span className='sr-only'>Loading...</span>
 				</div>
-			)
-		} else {
-			return (
-				<>
-					<h1 align='center'>Inbox</h1>
-					<ReadMessage></ReadMessage>
-					<Table striped bordered hover>
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Subject</th>
-								<th>From</th>
-							</tr>
-						</thead>
-						<tbody>
-							{this.state.data.map((msg, index) => {
-								return (
-									<tr key={index} onClick={this.readMessageModal}>
-										<td>{index}</td>
-										<td>{msg.Subject}</td>
-										<td>{msg.SourceEmail}</td>
-									</tr>
-								)
-							})}
-						</tbody>
-					</Table>
-				</>
-			)
-		}
+			</div>
+		)
+	} else {
+		return (
+			<>
+				<Modal show={show} onHide={handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>Message</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>Woohoo, youre reading this text in a modal!</Modal.Body>
+					<Modal.Footer>
+						<Button variant='secondary' onClick={handleClose}>
+							Close
+						</Button>
+						<Button variant='primary' onClick={handleClose}>
+							Save Changes
+						</Button>
+					</Modal.Footer>
+				</Modal>
+				<h1 align='center'>Inbox</h1>
+				<Table striped bordered hover>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Subject</th>
+							<th>From</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data.map((msg, index) => {
+							return (
+								<tr key={index} onClick={handleShow}>
+									<td>{index}</td>
+									<td>{msg.Subject}</td>
+									<td>{msg.SourceEmail}</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</Table>
+			</>
+		)
 	}
 }
 
